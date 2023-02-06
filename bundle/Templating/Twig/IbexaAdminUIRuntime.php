c99f97fb6c5a87f91d863071c118c606b27ce5b2
Petar Jakopec <petar.jakopec0@gmail.com>
2023-02-06 12:42:20 +0100

    NGSTACK-658 add Twig extension for content type count

---
+++ bundle/Templating/Twig/IbexaAdminUIRuntime.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\BetterIbexaAdminUIBundle\Templating\Twig;
+
+use eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use Ibexa\Contracts\Core\Repository\Exceptions\InvalidArgumentException;
+use Netgen\IbexaSiteApi\API\FilterService;
+use Twig\Extension\RuntimeExtensionInterface;
+
+class IbexaAdminUIRuntime implements RuntimeExtensionInterface
+{
+    private FilterService $filterService;
+
+    /**
+     * @param \Netgen\IbexaSiteApi\API\FilterService $filterService
+     */
+    public function __construct(FilterService $filterService)
+    {
+        $this->filterService = $filterService;
+    }
+
+    public function countByContentType(string $contentTypeIdentifier): ?int
+    {
+        $query = new Query();
+        $query->filter = new Criterion\ContentTypeIdentifier($contentTypeIdentifier);
+        $query->limit = 0;
+
+        try {
+            return $this->filterService->filterContent($query)->totalCount;
+        } catch (InvalidArgumentException $e) {
+            return null;
+        }
+    }
+}
