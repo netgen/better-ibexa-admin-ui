a286c1034475ed5393aa0b644089aae8410b51bd
Petar Jakopec <petar.jakopec0@gmail.com>
2023-02-06 14:01:21 +0100

    NGSTACK-658 add compiler pass to register ngadmin theme with priority

--- bundle/NetgenBetterIbexaAdminUIBundle.php
+++ bundle/NetgenBetterIbexaAdminUIBundle.php
@@ -1,9 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Netgen\Bundle\BetterIbexaAdminUIBundle;
 
+use Netgen\Bundle\BetterIbexaAdminUIBundle\DependencyInjection\CompilerPass;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class NetgenBetterIbexaAdminUIBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(new CompilerPass\AdminDesignPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 1000);
+    }
 }
