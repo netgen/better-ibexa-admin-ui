9439bb22ab1758e6f7d5809bf9bd63d7e56f1c44
Petar Jakopec <petar.jakopec0@gmail.com>
2023-02-07 14:00:18 +0100

    NGSTACK-658 remove try-catch exception handling

--- bundle/Templating/Twig/IbexaAdminUIRuntime.php
+++ bundle/Templating/Twig/IbexaAdminUIRuntime.php
@@ -6,7 +6,6 @@ namespace Netgen\Bundle\BetterIbexaAdminUIBundle\Templating\Twig;
 
 use eZ\Publish\API\Repository\Values\Content\Query;
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
-use Ibexa\Contracts\Core\Repository\Exceptions\InvalidArgumentException;
 use Netgen\IbexaSiteApi\API\FilterService;
 use Twig\Extension\RuntimeExtensionInterface;
 
@@ -19,16 +18,12 @@ class IbexaAdminUIRuntime implements RuntimeExtensionInterface
         $this->filterService = $filterService;
     }
 
-    public function countByContentType(string $contentTypeIdentifier): ?int
+    public function countByContentType(string $contentTypeIdentifier): int
     {
         $query = new Query();
         $query->filter = new Criterion\ContentTypeIdentifier($contentTypeIdentifier);
         $query->limit = 0;
 
-        try {
-            return $this->filterService->filterContent($query)->totalCount;
-        } catch (InvalidArgumentException $e) {
-            return null;
-        }
+        return $this->filterService->filterContent($query)->totalCount;
     }
 }
