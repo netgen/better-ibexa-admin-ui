b1dea463770dc45445e539935d9621624a129456
Edi Modrić <edi.modric@gmail.com>
2023-02-14 14:02:16 +0100

    Use ngadmin design instead of ngadmin theme

--- bundle/DependencyInjection/NetgenBetterIbexaAdminUIExtension.php
+++ bundle/DependencyInjection/NetgenBetterIbexaAdminUIExtension.php
@@ -5,19 +5,47 @@ declare(strict_types=1);
 namespace Netgen\Bundle\BetterIbexaAdminUIBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\DelegatingLoader;
+use Symfony\Component\Config\Loader\LoaderResolver;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\GlobFileLoader;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Yaml\Yaml;
 
-final class NetgenBetterIbexaAdminUIExtension extends Extension
+use function file_get_contents;
+
+final class NetgenBetterIbexaAdminUIExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new YamlFileLoader(
-            $container,
-            new FileLocator(__DIR__ . '/../Resources/config'),
+        $locator = new FileLocator(__DIR__ . '/../Resources/config');
+
+        $loader = new DelegatingLoader(
+            new LoaderResolver(
+                [
+                    new GlobFileLoader($container, $locator),
+                    new YamlFileLoader($container, $locator),
+                ],
+            ),
         );
 
-        $loader->load('templating.yaml');
+        $loader->load('services/*.yaml', 'glob');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $configs = [
+            'design.yaml' => 'ibexa_design_engine',
+        ];
+
+        foreach ($configs as $fileName => $extensionName) {
+            $configFile = __DIR__ . '/../Resources/config/' . $fileName;
+            $config = Yaml::parse((string) file_get_contents($configFile));
+            $container->prependExtensionConfig($extensionName, $config);
+            $container->addResource(new FileResource($configFile));
+        }
     }
 }
