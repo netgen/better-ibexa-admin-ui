52a2f01d0aedd310aa8503ea793e8b9c61980915
Petar Španja <petar@spanja.info>
2023-03-22 08:28:22 +0100

    NGSTACK-743: use composition instead of inheritance

--- bundle/Tab/LocationView/TranslationsTab.php
+++ bundle/Tab/LocationView/TranslationsTab.php
@@ -5,49 +5,56 @@ declare(strict_types=1);
 namespace Netgen\Bundle\BetterIbexaAdminUIBundle\Tab\LocationView;
 
 use Ibexa\AdminUi\Tab\LocationView\TranslationsTab as IbexaTranslationsTab;
-use Ibexa\AdminUi\UI\Dataset\DatasetFactory;
-use Ibexa\Contracts\Core\Repository\LanguageService;
+use Ibexa\Contracts\AdminUi\Tab\AbstractEventDispatchingTab;
+use Ibexa\Contracts\AdminUi\Tab\OrderedTabInterface;
 use Ibexa\Contracts\Core\Repository\PermissionResolver;
 use Netgen\Bundle\BetterIbexaAdminUIBundle\Form\FormFactory;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\Form\FormFactoryInterface;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment;
 
-final class TranslationsTab extends IbexaTranslationsTab
+final class TranslationsTab extends AbstractEventDispatchingTab implements OrderedTabInterface
 {
     public function __construct(
-        Environment $twig,
-        TranslatorInterface $translator,
-        DatasetFactory $datasetFactory,
-        UrlGeneratorInterface $urlGenerator,
-        EventDispatcherInterface $eventDispatcher,
-        FormFactoryInterface $formFactory,
-        LanguageService $languageService,
+        private readonly IbexaTranslationsTab $inner,
         private readonly PermissionResolver $permissionResolver,
         private readonly FormFactory $localFormFactory,
+        Environment $twig,
+        TranslatorInterface $translator,
+        EventDispatcherInterface $eventDispatcher,
     ) {
-        parent::__construct(
-            $twig,
-            $translator,
-            $datasetFactory,
-            $urlGenerator,
-            $eventDispatcher,
-            $formFactory,
-            $permissionResolver,
-            $languageService
-        );
+        parent::__construct($twig, $translator, $eventDispatcher);
+    }
+
+    public function getIdentifier(): string
+    {
+        return $this->inner->getIdentifier();
+    }
+
+    public function getName(): string
+    {
+        return $this->inner->getName();
+    }
+
+    public function getOrder(): int
+    {
+        return $this->inner->getOrder();
+    }
+
+    public function getTemplate(): string
+    {
+        return $this->inner->getTemplate();
     }
 
     public function getTemplateParameters(array $contextParameters = []): array
     {
+        $parentParameters = $this->inner->getTemplateParameters($contextParameters);
+
         /** @var \Ibexa\Contracts\Core\Repository\Values\Content\Content $content */
         $content = $contextParameters['content'];
-        $parentParameters = parent::getTemplateParameters($contextParameters);
 
         $alwaysAvailableUpdateForm = $this->localFormFactory->createContentAlwaysAvailableUpdateForm(
-            $content->contentInfo
+            $content->contentInfo,
         );
         $canEdit = $this->permissionResolver->canUser(
             'content',
