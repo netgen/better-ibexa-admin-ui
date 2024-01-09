117b86d099840b6dd7ef2e85578ccba6bc78c005
Petar Španja <petar@spanja.info>
2024-01-09 11:42:16 +0100

    NGSTACK-819: reconfigure services to use FilterService search adapter from Netgen Site API

--- bundle/NetgenBetterIbexaAdminUIBundle.php
+++ bundle/NetgenBetterIbexaAdminUIBundle.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace Netgen\Bundle\BetterIbexaAdminUIBundle;
 
+use Netgen\Bundle\BetterIbexaAdminUIBundle\DependencyInjection\Compiler\SearchOverridePass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class NetgenBetterIbexaAdminUIBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(new SearchOverridePass());
+    }
 }
