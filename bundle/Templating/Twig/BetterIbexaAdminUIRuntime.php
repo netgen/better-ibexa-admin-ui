95533075900794fb04c973b862fd3fb989f42f60
Miljenko Muha <miljenko@netgen.io>
2024-02-08 14:00:50 +0100

    NGSTACK-829 removed unnecessary service

--- bundle/Templating/Twig/BetterIbexaAdminUIRuntime.php
+++ bundle/Templating/Twig/BetterIbexaAdminUIRuntime.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Netgen\Bundle\BetterIbexaAdminUIBundle\Templating\Twig;
 
 use Ibexa\Contracts\Core\Repository\ContentService;
-use Ibexa\Contracts\Core\Repository\LanguageService;
 use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion;
 use Ibexa\Contracts\Core\Repository\Values\Filter\Filter;
 use Twig\Extension\RuntimeExtensionInterface;
@@ -13,12 +12,10 @@ use Twig\Extension\RuntimeExtensionInterface;
 final class BetterIbexaAdminUIRuntime implements RuntimeExtensionInterface
 {
     private ContentService $contentService;
-    private LanguageService $languageService;
 
-    public function __construct(ContentService $contentService, LanguageService $languageService)
+    public function __construct(ContentService $contentService)
     {
         $this->contentService = $contentService;
-        $this->languageService = $languageService;
     }
 
     public function countContentByContentType(string $contentTypeIdentifier): int
