83445be5ce5de8cfd327f900efa01211579ba75c
Miljenko Muha <miljenko@netgen.io>
2024-02-08 11:49:10 +0100

    NGSTACK-829 removed dependency to nglayouts, flags replaced by names of languages

--- bundle/Templating/Twig/BetterIbexaAdminUIExtension.php
+++ bundle/Templating/Twig/BetterIbexaAdminUIExtension.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Netgen\Bundle\BetterIbexaAdminUIBundle\Templating\Twig;
 
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 final class BetterIbexaAdminUIExtension extends AbstractExtension
@@ -15,6 +16,16 @@ final class BetterIbexaAdminUIExtension extends AbstractExtension
             new TwigFunction(
                 'ng_count_content_by_content_type',
                 [BetterIbexaAdminUIRuntime::class, 'countContentByContentType'],
+            ),
+        ];
+    }
+
+    public function getFilters()
+    {
+        return [
+            new TwigFunction(
+                'show_language_name',
+                [BetterIbexaAdminUIRuntime::class, 'getLanguageName'],
             ),
         ];
     }
--- bundle/Templating/Twig/BetterIbexaAdminUIRuntime.php
+++ bundle/Templating/Twig/BetterIbexaAdminUIRuntime.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Netgen\Bundle\BetterIbexaAdminUIBundle\Templating\Twig;
 
 use Ibexa\Contracts\Core\Repository\ContentService;
+use Ibexa\Contracts\Core\Repository\Exceptions\NotFoundException;
+use Ibexa\Contracts\Core\Repository\LanguageService;
 use Ibexa\Contracts\Core\Repository\Values\Content\Query\Criterion;
 use Ibexa\Contracts\Core\Repository\Values\Filter\Filter;
 use Twig\Extension\RuntimeExtensionInterface;
@@ -12,10 +14,12 @@ use Twig\Extension\RuntimeExtensionInterface;
 final class BetterIbexaAdminUIRuntime implements RuntimeExtensionInterface
 {
     private ContentService $contentService;
+    private LanguageService $languageService;
 
-    public function __construct(ContentService $contentService)
+    public function __construct(ContentService $contentService, LanguageService $languageService)
     {
         $this->contentService = $contentService;
+        $this->languageService = $languageService;
     }
 
     public function countContentByContentType(string $contentTypeIdentifier): int
@@ -25,5 +29,14 @@ final class BetterIbexaAdminUIRuntime implements RuntimeExtensionInterface
         $filter->withLimit(1);
 
         return $this->contentService->find($filter)->getTotalCount() ?? 0;
+    }
+
+    /**
+     * @throws NotFoundException
+     */
+    public function getLanguageName(string $languageCode): string
+    {
+
+        return $this->languageService->loadLanguage($languageCode)->name;
     }
 }
